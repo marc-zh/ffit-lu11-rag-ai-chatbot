f884c7d84e015a6c1a2ab0a31acc9e6c506d8d97
marc-zh <holenstein.marc@mail.ch>
2025-12-30 12:00:00 +0100

    Add basic project files

---
+++ src/main/java/ch/bzz/rag/ApplicationMain.java
@@ -0,0 +1,12 @@
+package ch.bzz.rag;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ApplicationMain {
+    public static void main(String[] args) {
+        log.info("Application has been started");
+    }
+}
